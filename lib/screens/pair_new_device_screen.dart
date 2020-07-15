d2d85b09a03309d55de357d21c82ec64c7ef6af5
Khalid War <git@khalidwar.com>
2020-07-15 10:52:02 -0500

    Changes to be committed: 	modified:   lib/screens/pair_new_device_screen.dart

--- lib/screens/pair_new_device_screen.dart
+++ lib/screens/pair_new_device_screen.dart
@@ -14,73 +14,54 @@ class PairNewDevice extends StatefulWidget {
 class _PairNewDeviceState extends State<PairNewDevice> {
   @override
   Widget build(BuildContext context) {
-    var height = MediaQuery.of(context).size.height;
-    var width = MediaQuery.of(context).size.width;
-    return Column(
-      children: [
-        Card(
-          margin: EdgeInsets.symmetric(
-              horizontal: width * 0.07, vertical: height * 0.3),
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(20)),
-              side: BorderSide(
-                  style: BorderStyle.solid,
-                  color: Theme.of(context).accentColor)),
-          child: Padding(
-            padding: EdgeInsets.all(15),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  'Other devices running KDE Connect in your same network should appear here.',
-                ),
-                SizedBox(height: 20),
-                Text('Connected Devices',
-                    style:
-                        TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-                Divider(
-                  endIndent: 25,
-                  height: 2,
-                  color:
-                      isLightTheme(context) ? Colors.black54 : Colors.white54,
-                ),
-                SizedBox(height: 5),
-                Flexible(
-                  child: ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: connectedDevicesList.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return DevicesListTile(
-                        label: connectedDevicesList[index].label,
-                        icon: connectedDevicesList[index].icon,
-                        isConnected: connectedDevicesList[index].isConnected,
-                        onPressed: () {
-                          showModal(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return DeviceSimpleDialog();
-                            },
-                          );
+    return Scaffold(
+      appBar: AppBar(title: Text('Pair New Device')),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+                'Other devices running KDE Connect in your same network should appear here.',
+                style: TextStyle(fontSize: 15)),
+            SizedBox(height: 20),
+            Text('Connected Devices',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+            Divider(
+              endIndent: 25,
+              height: 2,
+              color: isLightTheme(context) ? Colors.black54 : Colors.white54,
+            ),
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: connectedDevicesList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return DevicesListTile(
+                    label: connectedDevicesList[index].label,
+                    icon: connectedDevicesList[index].icon,
+                    isConnected:
+                        connectedDevicesList[index].isConnected ? true : false,
+                    onPressed: () {
+                      showModal(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return DeviceSimpleDialog();
                         },
                       );
                     },
-                  ),
-                ),
-                SizedBox(height: 20),
-                Center(
-                  child: FlatButton(
-                    child: Text('Close'),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                )
-              ],
+                  );
+                },
+              ),
             ),
-          ),
+            Container(
+              child: Text(
+                'Please make sure both devices are on the same network and neither of them is on VPN.',
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
