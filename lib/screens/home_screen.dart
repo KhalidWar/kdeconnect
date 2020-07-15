2b936b88106754b8e54e8b4c1490619946899c20
Khalid War <git@khalidwar.com>
2020-07-14 22:04:58 -0500

    Restructured files and checked off few todos.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:kdeconnect/screens/media_control_tab.dart';
@@ -27,7 +28,11 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: tabsList[_selectedIndex].widget,
+      body: PageTransitionSwitcher(
+          duration: Duration(milliseconds: 150),
+          transitionBuilder: (child, primaryAnimation, secondaryAnimation) =>
+              FadeScaleTransition(animation: primaryAnimation, child: child),
+          child: tabsList[_selectedIndex].widget),
       appBar: AppBar(
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         elevation: 0,
@@ -68,12 +73,12 @@ class _HomeScreenState extends State<HomeScreen> {
         currentIndex: _selectedIndex,
         onTap: selectedIndex,
         type: BottomNavigationBarType.fixed,
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        backgroundColor: Theme.of(context).primaryColorLight,
         selectedItemColor: Theme.of(context).accentColor,
         showSelectedLabels: false,
         showUnselectedLabels: false,
-        selectedIconTheme: IconThemeData(size: 25),
-        unselectedIconTheme: IconThemeData(size: 23),
+        selectedIconTheme: IconThemeData(size: 30),
+        unselectedIconTheme: IconThemeData(size: 25),
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.insert_drive_file),
